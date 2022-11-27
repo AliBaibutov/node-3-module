5abbf48f61bb6dc3a4eb13154f19b598075e8767
AliKhan <baybutov95@mail.ru>
2022-11-27 14:53:17 +0300

    task

--- public/app.js
+++ public/app.js
@@ -7,6 +7,28 @@ document.addEventListener("click", (event) => {
   }
 });
 
+// const divTitle = document.getElementsByClassName(
+//   "me-auto p-2 bd-highlight"
+// ).innerText;
+// console.log(divTitle);
+
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.type === "edit") {
+    const id = event.target.dataset.id;
+    // let title = event.target.dataset.content;
+    const newTitle = prompt("Введите новое название");
+    newTitle
+      ? edit(id, newTitle).then(() => {
+          event.target.previousElementSibling.innerText = newTitle;
+        })
+      : null;
+  }
+});
+
 async function remove(id) {
   await fetch(`/${id}`, { method: "DELETE" });
+}
+
+async function edit(id, title) {
+  await fetch(`/${id}/${title}`, { method: "PUT" });
 }
